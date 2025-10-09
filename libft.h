b903bd9ee7709f448d1e55e531b7c5cb236d21c4
Maurice Lorenz <mlorenz@student.42heilbronn.de>
2025-10-09 20:44:20 +0200

    Add string.h and strings.h to libft.h

--- libft.h
+++ libft.h
@@ -6,11 +6,13 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 12:10:44 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/10/09 12:10:54 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/10/09 16:47:43 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+# include <string.h>
+# include <strings.h>
 #endif
