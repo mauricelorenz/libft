f9b7e0e49a8bb9d51ff1e22f977ebdde1d10f6d6
Maurice Lorenz <me@mauricelorenz.com>
2025-10-13 21:27:36 +0200

    Add ft_strlcpy.c

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ SRC =	ft_bzero.c \
 		ft_memcpy.c \
 		ft_memmove.c \
 		ft_memset.c \
+		ft_strlcpy.c \
 		ft_strlen.c
 
 OBJ =	$(SRC:.c=.o)
---
+++ ft_strlcpy.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 20:11:53 by mlorenz           #+#    #+#             */
+/*   Updated: 2025/10/13 21:22:54 by mlorenz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (!size)
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int	main(void)
+// {
+// 	const char	src[] = "";
+// 	char		ft_dst[20];
+// 	char		st_dst[20];
+// 	int			ft_return = ft_strlcpy(ft_dst, src, 10);
+// 	int			st_return = strlcpy(st_dst, src, 10);
+
+// 	printf("src:    %s\nft_dst: %s\nst_dst: %s\nst_return: %i\nst_return: %i\n",
+// 		src, ft_dst, st_dst, ft_return, st_return);
+// }
+
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (s[i])
+// 		i++;
+// 	return (i);
+// }
