e5fb54d53e110caf8f4dd303037c32d3a8450355
Maurice Lorenz <me@mauricelorenz.com>
2025-10-13 20:10:29 +0200

    Fix off-by-one error in ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 15:42:57 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/10/13 17:49:24 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/10/13 19:55:16 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,24 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 			*char_dest++ = *char_src++;
 	else
 		while (i++ < n)
-			*((char_dest--) + n) = *((char_src--) + n);
+			*((--char_dest) + n) = *((--char_src) + n);
 	return (dest);
 }
 
 // #include <stdio.h>
+// #include <string.h>
 
 // int	main(void)
 // {
 // 	char	s[] = "1234567890";
+// 	char	t[] = "1234567890";
+// 	char	u[] = "1234567890";
+// 	char	v[] = "1234567890";
 
 // 	ft_memmove(s + 4, s + 3, 3);
-// 	printf("%s\n", s);
+// 	memmove(t + 4, t + 3, 3);
+// 	ft_memmove(u + 3, u + 4, 3);
+// 	memmove(v + 3, v + 4, 3);
+
+// 	printf("ft: %s\nst: %s\nft: %s\nst: %s\n", s, t, u, v);
 // }
