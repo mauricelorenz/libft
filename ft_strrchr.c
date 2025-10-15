bc20c716b99c9fd5da747f2686af810416082474
Maurice Lorenz <me@mauricelorenz.com>
2025-10-15 11:44:31 +0200

    Refactor ft_strchr.c to use const char * and apply s_orig naming convention

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,20 +6,20 @@
 /*   By: mlorenz <mlorenz@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 21:39:53 by mlorenz           #+#    #+#             */
-/*   Updated: 2025/10/14 22:06:17 by mlorenz          ###   ########.fr       */
+/*   Updated: 2025/10/15 11:31:16 by mlorenz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*ptr;
+	const char	*s_orig;
 
-	ptr = (char *)s;
+	s_orig = s;
 	while (*s)
 		s++;
 	if ((unsigned char)c == '\0')
 		return ((char *)s);
-	while (s >= ptr)
+	while (s >= s_orig)
 	{
 		if (*s == (unsigned char)c)
 			return ((char *)s);
